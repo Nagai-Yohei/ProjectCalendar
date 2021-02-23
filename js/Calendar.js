1a745b7c8e8966a5457166ff1fb587de6201378d
Nagai-Yohei <nagai.yohei@gmail.com>
2021-02-23 17:17:19 +0900

    複数月を表示できるように修正

--- js/Calendar.js
+++ js/Calendar.js
@@ -1,30 +1,16 @@
 
 const date = new Date()
-
 const year = date.getFullYear()
 const month = date.getMonth() + 1
+const config = {
+    show: 3,
+}
 
-// 月の最初の日と曜日を取得
-const startDate = new Date(year, month - 1, 1)
-const startDay = startDate.getDay()
-
-// 月の最後の日を取得
-const endDate = new Date(year, month,  0)
-const endDayCount = endDate.getDate()
-
-const lastMonthEndDate = new Date(year, month-1, 0)
-const lastMonthEndDayCount = lastMonthEndDate.getDate()
-
-
-let dayCount = 1
-let calendarHtml = ''
-let finishMonth = false
-
-function GetYearAndMonth(yearstr, monthstr) {
+function getYearAndMonth(year, month) {
     return ('<h1>' + year  + '/' + month + '</h1>')
 }
 
-function GetDayOfWeekTable(){
+function getDayOfWeekTable(){
     const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     let txt = ''
     for (let i = 0; i < weeks.length; i++) {
@@ -33,37 +19,61 @@ function GetDayOfWeekTable(){
     return txt
 }
 
-calendarHtml += GetYearAndMonth(year, month)
-calendarHtml += '<table>'
-calendarHtml += GetDayOfWeekTable()
-
-const tdOtherMonth = '<td class="is-other-month">'
-
-for (let w = 0; ; w++) {
-    calendarHtml += '<tr>'
-
-    for (let d = 0; d < 7; d++) {
-        if (w == 0 && d < startDay) {
-            // 1行目で1日の曜日の前
-            let num = lastMonthEndDayCount - startDay + d + 1
-            calendarHtml += '<td class="is-disabled">' + num + '</td>'
-        } else if (dayCount > endDayCount) {
-            // 末尾の日数を超えた
-            let num = dayCount - endDayCount
-            calendarHtml += '<td class="is-disabled">' + num + '</td>'
-            finishMonth = true
-            dayCount++
-        } else {
-            calendarHtml += '<td>' + dayCount + '</td>'
-            dayCount++
+function createCalendar(year, month) {
+    const startDate = new Date(year, month - 1, 1)
+    const startDay = startDate.getDay()
+    const endDate = new Date(year, month,  0)
+    const endDayCount = endDate.getDate()
+    const lastMonthEndDate = new Date(year, month-1, 0)
+    const lastMonthEndDayCount = lastMonthEndDate.getDate()
+    
+    let dayCount = 1
+    let calendarHtml = ''
+    let finishMonth = false
+    
+    calendarHtml += getYearAndMonth(year, month)
+    calendarHtml += '<table>'
+    calendarHtml += getDayOfWeekTable()
+    
+    for (let w = 0; ; w++) {
+        calendarHtml += '<tr>'
+        for (let d = 0; d < 7; d++) {
+            if (w == 0 && d < startDay) {
+                let num = lastMonthEndDayCount - startDay + d + 1
+                calendarHtml += '<td class="is-disabled">' + num + '</td>'
+            } else if (dayCount > endDayCount) {
+                let num = dayCount - endDayCount
+                calendarHtml += '<td class="is-disabled">' + num + '</td>'
+                finishMonth = true
+                dayCount++
+            } else {
+                calendarHtml += '<td>' + dayCount + '</td>'
+                dayCount++
+            }
+        }
+        calendarHtml += '</tr>'
+    
+        if (finishMonth) {
+            break
         }
     }
-    calendarHtml += '</tr>'
+    calendarHtml += '</table>'
+    return calendarHtml
+}
 
-    if (finishMonth) {
-        break
+function showCalendar(year, month) {
+    for (let i = 0; i < config.show; i++) {
+        const calendarHtml = createCalendar(year, month)
+        const sec = document.createElement('section')
+        sec.innerHTML = calendarHtml
+        document.querySelector('#calendar').appendChild(sec)
+
+        month++
+        if (month > 12) {
+            year++
+            month = 1
+        }
     }
 }
-calendarHtml += '</table>'
 
-document.querySelector('#calendar').innerHTML = calendarHtml
+showCalendar(year, month)
