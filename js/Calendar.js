4e8ae94ac7199f41be0e873f1eaf5c9bbb434f4f
Nagai-Yohei <nagai.yohei@gmail.com>
2021-02-23 17:22:08 +0900

    プロジェクト名を入れられるように変更

--- js/Calendar.js
+++ js/Calendar.js
@@ -6,20 +6,8 @@ const config = {
     show: 3,
 }
 
-function getYearAndMonth(year, month) {
-    return ('<h1>' + year  + '/' + month + '</h1>')
-}
-
-function getDayOfWeekTable(){
-    const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    let txt = ''
-    for (let i = 0; i < weeks.length; i++) {
-        txt += '<td>' + weeks[i] + '</td>'
-    }
-    return txt
-}
-
 function createCalendar(year, month) {
+    const weeks = ['project', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     const startDate = new Date(year, month - 1, 1)
     const startDay = startDate.getDay()
     const endDate = new Date(year, month,  0)
@@ -31,13 +19,15 @@ function createCalendar(year, month) {
     let calendarHtml = ''
     let finishMonth = false
     
-    calendarHtml += getYearAndMonth(year, month)
+    calendarHtml += '<h1>' + year  + '/' + month + '</h1>'
     calendarHtml += '<table>'
-    calendarHtml += getDayOfWeekTable()
+    for (let i = 0; i < weeks.length; i++) {
+        calendarHtml += '<td>' + weeks[i] + '</td>'
+    }
     
     for (let w = 0; ; w++) {
-        calendarHtml += '<tr>'
-        for (let d = 0; d < 7; d++) {
+        calendarHtml += '<tr><td></td>'
+        for (let d = 1; d < weeks.length; d++) {
             if (w == 0 && d < startDay) {
                 let num = lastMonthEndDayCount - startDay + d + 1
                 calendarHtml += '<td class="is-disabled">' + num + '</td>'
